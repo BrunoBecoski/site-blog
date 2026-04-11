06617f9e9604f71da0e873edcaaa733524fa6a84
Bruno Becoski <brunobecoski@gmail.com>
2026-04-11 18:21:22 -0300

    Cria o componente CustomerStorySection

---
+++ src/components/customer-story-section/customer-story-section.tsx
@@ -0,0 +1,71 @@
+import { PT_Sans_Caption } from "next/font/google"
+import Image from "next/image"
+
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ['latin'],
+  weight: '700',
+})
+
+const customersStories = [
+  {
+    id: '7631610e-7eb0-4f1d-bdcf-6333ba6ed160',
+    content: 'Criar minha loja com o site.set foi a melhor decisão para o meu negócio. A plataforma é super intuitiva, e consegui colocar meus produtos à venda em poucos minutos.',
+    author: {
+      name: 'Annette Bones',
+      role: 'CEO na Anne Corp',
+      avatar: '/customer-01.png',
+    }
+  }, {
+    id: 'cdd88ec2-66b2-4ec1-bbc4-2e9d3f545093',
+    content: 'Transformar minha ideia em uma loja online foi fácil e rápido. Adorei as opções de personalização e a simplicidade para gerenciar os pedidos. Já vejo meus produtos alcançando mais pessoas!.',
+    author: {
+      name: 'Jacob Jones',
+      role: 'CEO na JJ Org',
+      avatar: '/customer-02.png',
+    }
+  }
+]
+
+export const CustomerStorySection = () => {
+  return (
+    <section className="container py-8 md:py-10">
+      <div className="flex flex-col items-center gap-12">
+        <h2 className={`${ptSansCaption.className} text-heading-xl text-gray-100`}>
+          Quem utiliza, aprova!
+        </h2>
+
+        <div className="grid gap-8 md:grid-cols-2">
+          {customersStories.map((customer) => (
+            <div key={customer.id} className="flex flex-col gap-6 justify-between rounded-xl bg-gray-500 border border-gray-400 p-6 md:p-12">
+              <p className="text-balance text-gray-200 italic">
+                {customer.content}
+              </p>
+
+              <div className="flex items-center gap-3">
+                <div className="relative size-10 overflow-hidden rounded-full border-2 border-blue-200">
+                  <Image 
+                    src={customer.author.avatar}
+                    alt={customer.author.name}
+                    width={40}
+                    height={40}
+                    className="object-cover"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <strong className="text-sm text-gray-200">
+                    {customer.author.name}
+                  </strong>
+                  <span className="text-xs text-gray-300">
+                    {customer.author.role}
+                  </span>
+                </div>
+              </div>
+
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from "@/components/hero-section";
 import { FeatureSection } from "@/components/feature-section";
 import { SupportSection } from "@/components/support-section";
+import { CustomerStorySection } from "@/components/customer-story-section";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
         <FeatureSection />
 
         <SupportSection />
+
+        <CustomerStorySection />
       </article>
     </>
   );
