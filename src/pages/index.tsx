6b271c76e782139080288764fbd8935b6d214324
Bruno Becoski <brunobecoski@gmail.com>
2026-04-14 19:44:28 -0300

    Cria o primero post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { allPosts } from "contentlayer/generated";
+
 import { LandingPage } from "@/templates/landing-page";
 
 export default function Home() {
+  console.log(allPosts)
+  
   return (
     <LandingPage />
   );
