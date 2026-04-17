6d1c751cc7d3cf22dd7f9595ad9f4af9ac7f1e95
Bruno Becoski <brunobecoski@gmail.com>
2026-04-17 19:28:33 -0300

    Cria o componente PostGridCard

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -3,11 +3,12 @@ import { useRouter } from "next/router";
 
 import { Search } from "./components/search";
 import { PostCard } from "./components/post-card";
+import { PostGridCard } from "./components/post-grid-card";
 
 export function BlogList() {
   const router = useRouter()
   const querySearch = router.query.search as string
-  const pageTitle = querySearch 
+  const pageTitle = querySearch
     ? `Resultados de busca para: "${querySearch}"`
     : 'Dicas e estratégias para impulsionar seu negócio'
 
@@ -17,7 +18,7 @@ export function BlogList() {
         <title>Site.set | Blog</title>
       </Head>
 
-      <header>
+      <header className="pb-14">
         <div className="container space-y-6 md:space-y-0 flex flex-col items-start justify-between md:flex-row md:items-end">
           <div className="flex flex-col gap-4">
             <span className="uppercase text-body-tag text-cyan-100 bg-cyan-300 py-2 px-3 w-fit rounded-sm">
@@ -28,22 +29,24 @@ export function BlogList() {
               {pageTitle}
             </h1>
           </div>
-          
+
           <Search />
         </div>
       </header>
 
-      <PostCard
-      slug="transformando-seu-negocio-em-uma-loja-virtual"
-        date="20/12/2024"
-        title="Transformando seu negócio em uma loja virtual"
-        description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
-        image="/assets/primeiro-post.png"
-        author={{
-          name: 'Aspen Dokidis',
-          avatar: '/customer-03.png',
-        }}
-      />
+      <PostGridCard>
+        <PostCard
+          slug="transformando-seu-negocio-em-uma-loja-virtual"
+          date="20/12/2024"
+          title="Transformando seu negócio em uma loja virtual"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+          image="/assets/primeiro-post.png"
+          author={{
+            name: 'Aspen Dokidis',
+            avatar: '/customer-03.png',
+          }}
+        />
+      </PostGridCard>
     </div>
   )
 }
